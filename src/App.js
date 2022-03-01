9bec587e84640d6b927f77eb12d0a417759ae674
ZachCodedThat <Zachemailsback@gmail.com>
2022-03-01 12:25:44 -0500

    added filters
    This is a test to see how much bs I can write
    This is a test to see how much bs I can writeThis is a test to see how much bs I can writeThis is a test to see how much bs I can write
    This is a test to see how much bs I can write
    This is a test to see how much bs I can write

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import supabase from "./supabase.png";
 import "./App.css";
 import { useQuery, gql } from "@apollo/client";
 import { useState, useEffect } from "react";
@@ -7,7 +7,8 @@ import Fuse from "fuse.js";
 
 function App() {
   const [filter, setFilter] = useState("");
-  const [queryResults, setQueryResults] = useState([]);
+
+  const [queryResults, setQueryResults] = useState([]); // I an stupid and thought this would be an {} by default but nooooo it is a [] of {}'s
 
   const handleChange = (event) => setFilter(event.target.value);
 
@@ -48,7 +49,7 @@ function App() {
         distance: 200,
         maxPatternLength: 32,
         minMatchCharLength: 2,
-        keys: ["title"],
+        keys: ["title", "turnin.name"],
       };
 
       const fuse = new Fuse(data.quests, options);
@@ -57,13 +58,29 @@ function App() {
     }
   }, [data, filter]);
 
-  console.log(queryResults);
-
   const QuestItem = ({ quest }) => {
     return (
       <div className="quest-item">
         <span>
           <h3>{quest.title}</h3>
+          {quest.objectives
+            .filter(
+              (questRequirements) =>
+                questRequirements.targetItem !== null &&
+                questRequirements.targetItem.name !== "MS2000 Marker" &&
+                questRequirements.targetItem.name !== "WI-FI Camera" &&
+                questRequirements.targetItem.name !== "Signal Jammer"
+            )
+            .map((questRequirements) => (
+              <div>
+                <img src={questRequirements.targetItem.imageLink} />
+                <span>
+                  {questRequirements.targetItem.name != null
+                    ? ` X ${questRequirements.number}`
+                    : null}
+                </span>
+              </div>
+            ))}
           <h3>{quest.id}</h3>
           <h3>{quest.turnin.name}</h3>
         </span>
@@ -75,7 +92,7 @@ function App() {
     return (
       <div className="App">
         <div>
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={supabase} className="App-logo" alt="logo" />
           <p>YUUUUUUUUUURRRRRT</p>
           <p>Loading...</p>
         </div>
@@ -86,7 +103,7 @@ function App() {
     return (
       <div className="App">
         <div>
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={supabase} className="App-logo" alt="logo" />
           <p>YUUUUUUUUUURRRRRT</p>
           <p>Error :`(`</p>
         </div>
@@ -96,7 +113,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={supabase} className="App-logo" alt="logo" />
         <p>YUUUUUUUUUURRRRRT</p>
         <input type="text" value={filter} onChange={handleChange} />
         <div className="outerContainer">
