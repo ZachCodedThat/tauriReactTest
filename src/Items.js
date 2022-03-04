f1f763dd0e7146ccd02565d2fe2d3be543946fda
ZachCodedThat <Zachemailsback@gmail.com>
2022-03-04 12:15:36 -0500

    still working on changes to GQL calls

--- src/Items.js
+++ src/Items.js
@@ -3,53 +3,102 @@ import { useState, useEffect, useRef } from "react";
 import useDebounce from "./useDebounce";
 import Fuse from "fuse.js";
 import supabase from "./supabase.png";
+import NumberFormat from "react-number-format";
 
-import { ITEM_RESULTS } from "./Queries";
+import { ITEM_RESULTS, QUEST_RESULTS } from "./Queries";
 
 const ItemsQuery = () => {
   const [filter, setFilter] = useState("");
+  const [value, setValue] = useState("");
 
   const [queryResults, setQueryResults] = useState([]); // I an stupid and thought this would be an {} by default but nooooo it is a [] of {}'s
+  const [questItemIDResults, setQuestItemIDResults] = useState([]);
+  const [releventQuests, setReleventQuests] = useState([]);
 
   const searchInput = useRef();
   const handleFocus = () => {
     searchInput.current.focus();
   };
 
-  const handleChange = (event) => setFilter(event.target.value);
+  const handleClick = (event) => {
+    event.preventDefault();
+    setFilter(value);
+  };
+  const handleChange = (event) => setValue(event.target.value);
 
-  const debouncedFilter = useDebounce(filter, 750);
+  // fetches and such
 
   const { loading, error, data } = useQuery(ITEM_RESULTS, {
-    variables: { name: debouncedFilter },
+    variables: { name: filter },
   });
 
+  const itemID = data?.itemsByName.map((item) => item.id).toString();
+
+  // console.log(itemID, "itemID");
+
+  const { data: questData } = useQuery(QUEST_RESULTS);
+
+  const questItem = questData?.quests
+    .map((quest) => quest.objectives)
+    .flatMap((objectives) =>
+      objectives
+        .map((objective) => objective.target)
+        .filter((item) => item.includes(itemID))
+    )
+
+    .toString()
+    .includes(itemID);
+
+  console.log(questItem, "questItem");
+
+  const { data: releventQuestData } = useQuery(QUEST_RESULTS);
+
+  // console.log(releventQuests, "releventQuestData");
+
   useEffect(() => {
-    if (data) {
-      const options = {
-        includesMatches: true,
-        shouldSort: true,
-        threshold: 0.4,
-        location: 0,
-        distance: 200,
-        maxPatternLength: 32,
-        minMatchCharLength: 2,
-        keys: ["name"],
-      };
-      handleFocus();
+    if (filter !== "" && itemID !== undefined) {
+      setQueryResults(data.itemsByName);
+      setQuestItemIDResults(questData.quests);
 
-      const fuse = new Fuse(data.itemsByName, options);
-      const results = fuse.search(debouncedFilter);
-      setQueryResults(results.map((result) => result.item));
+      setReleventQuests(releventQuestData.quests);
     }
-  }, [data, debouncedFilter]);
+  }, [data]);
 
-  const Items = ({ item }) => {
+  // actual item component
+
+  const Items = ({ item, quest }) => {
+    // console.log(quest, "quest");
     return (
       <div className="quest-item">
         <span>
           <h2>{item.name}</h2>
+          <h4>{item.id}</h4>
           <img src={item.imageLink} alt={item.name} />
+
+          <div>
+            <NumberFormat
+              thousandSeparator={true}
+              thousandsGroupStyle="thousand"
+              prefix={"₽"}
+              value={item.avg24hPrice}
+              displayType={"text"}
+            />
+          </div>
+
+          {questItem ? (
+            <div>
+              <h2>Relevent Quests</h2>
+              <ul>
+                {quest.map((quest) =>
+                  quest.objectives.map((objectives) =>
+                    objectives.target.includes(itemID) ? (
+                      <li key={quest.id}>{quest.title}</li>
+                    ) : null
+                  )
+                )}
+              </ul>
+            </div>
+          ) : null}
         </span>
       </div>
     );
@@ -82,15 +131,18 @@ const ItemsQuery = () => {
       <div>
         <img src={supabase} className="App-logo" alt="logo" />
         <p>Enter item name</p>
-        <input
-          type="text"
-          value={filter}
-          onChange={handleChange}
-          ref={searchInput}
-        />
+        <form onSubmit={handleClick}>
+          <input
+            type="text"
+            value={value}
+            onChange={handleChange}
+            ref={searchInput}
+          />
+          <button type="submit">Search</button>
+        </form>
         <div className="outerContainer">
           {queryResults.map((item) => (
-            <Items key={item.id} item={item} />
+            <Items key={item.id} item={item} quest={releventQuests} />
           ))}
         </div>
       </div>
