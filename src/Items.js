df09692ce1e535925f7369aab5e0319175c1b1cd
ZachCodedThat <Zachemailsback@gmail.com>
2022-03-09 13:28:09 -0500

    Made buttons dynamic to what quest they show.

--- src/Items.js
+++ src/Items.js
@@ -1,8 +1,5 @@
 import { useQuery } from "@apollo/client";
 import { useState, useEffect, useRef } from "react";
-import useDebounce from "./useDebounce";
-import Fuse from "fuse.js";
-import killaGif from "./killaImage.gif";
 import KillaSticker from "./KillaSticker.png";
 import GluharSticker from "./GluharSticker.png";
 import TagilaSticker from "./TagilaSticker.png";
@@ -26,14 +23,7 @@ const ItemsQuery = () => {
   const [value, setValue] = useState("");
 
   const [queryResults, setQueryResults] = useState([]); // I an stupid and thought this would be an {} by default but nooooo it is a [] of {}'s
-  // const [questItemIDResults, setQuestItemIDResults] = useState([]);
   const [releventQuests, setReleventQuests] = useState([]);
-  // const [questItems, setQuestItems] = useState([]);
-
-  const searchInput = useRef();
-  const handleFocus = () => {
-    searchInput.current.focus();
-  };
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -41,47 +31,26 @@ const ItemsQuery = () => {
   };
   const handleChange = (event) => setValue(event.target.value);
 
-  // fetches and such
-
   const { loading, error, data } = useQuery(ITEM_RESULTS, {
     variables: { name: filter },
   });
 
   const itemID = data?.itemsByName.map((item) => item.id).toString();
 
-  // console.log(itemID, "itemID");
-
-  const { data: questData } = useQuery(QUEST_RESULTS);
-
-  // const questItem = questData?.quests
-  //   .map((quest) => quest.objectives)
-  //   .flatMap((objectives) => objectives.map((objective) => objective.target))
-
-  //   .toString()
-  //   .includes(itemID);
-
-  // console.log(questItems, "questItem");
-
   const { data: releventQuestData } = useQuery(QUEST_RESULTS);
-
-  // console.log(releventQuests, "releventQuestData");
 
   useEffect(() => {
     if (filter !== "" && itemID !== undefined) {
       setQueryResults(data.itemsByName);
-      // setQuestItemIDResults(questData.quests);
 
       setReleventQuests(releventQuestData.quests);
-      // setQuestItems(questItem);
     }
-  }, [data]);
+  }, [data, filter]);
 
   // actual item component
 
   const Items = ({ item, quest }) => {
-    const [show, setShow] = useState(false);
-
-    const handleToggle = () => setShow(!show);
+    const [editIndex, setEditIndex] = useState(null);
 
     return filter === "" ? null : (
       <div className="quest-item">
@@ -101,14 +70,21 @@ const ItemsQuery = () => {
           </h4>
           <img src={item.imageLink} alt={item.name} />
 
-          {quest.map((quest) =>
+          {quest.map((quest, index) =>
             quest.objectives.map((objectives) =>
               objectives.target.includes(item.id) ? (
                 <>
-                  <button onClick={handleToggle}>Relevant Quests</button>
-                  {show ? (
+                  <button
+                    onClick={() =>
+                      setEditIndex((editIndex) =>
+                        editIndex === index ? null : index
+                      )
+                    }
+                  >
+                    {quest.title}
+                  </button>
+                  {editIndex === index && (
                     <div className="relevent-quests">
-                      <h2>Relevent Quests</h2>
                       <ul>
                         <div className="quest" key={quest.id}>
                           <h3>Quest: {quest.title}</h3>
@@ -117,7 +93,7 @@ const ItemsQuery = () => {
                         </div>
                       </ul>
                     </div>
-                  ) : null}
+                  )}
                 </>
               ) : null
             )
@@ -142,7 +118,7 @@ const ItemsQuery = () => {
             ))}
           </span>
 
-          <p>Loading...</p>
+          <h2>Loading...</h2>
         </div>
       </div>
     );
@@ -160,7 +136,7 @@ const ItemsQuery = () => {
             />
           ))}
         </div>
-        <p>Error :`(`</p>
+        <h2>Error :`(`</h2>
       </div>
     );
 
@@ -176,16 +152,15 @@ const ItemsQuery = () => {
           />
         ))}
       </div>
-      <h1>Enter item name</h1>
+      <h1>Do I need this??</h1>
       <form onSubmit={handleClick}>
-        <input
-          type="text"
-          value={value}
-          onChange={handleChange}
-          ref={searchInput}
-        />
-        <button type="submit">Search</button>
+        <input type="text" value={value} onChange={handleChange} />
+        <button type="submit">Search for an item</button>
+        <button type="reset">Reset</button>
       </form>
+      <div>
+        <button onClick={() => setFilter("keycard")}>Keycards</button>
+      </div>
       <div className="outerContainer">
         {queryResults.map((item) => (
           <Items key={item.id} item={item} quest={releventQuests} />
